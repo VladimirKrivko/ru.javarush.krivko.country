2b129a3a46fc6f4c04b11f9bd6c23121bb7808a5
VladimirKrivko <mark.tatyschev@gmail.com>
2023-02-03 19:19:58 +0300

    first commit

---
+++ src/main/java/ru/javarush/krivko/App.java
@@ -0,0 +1,13 @@
+package ru.javarush.krivko;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
